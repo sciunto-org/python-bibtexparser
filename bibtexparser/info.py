dbabeef0988e07db4348c0a0bf4988e4ff1f1e49
François Boulogne <fboulogne sciunto org>
2013-01-06 21:02:07 +0100

    add info

---
+++ bibtexparser/info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Gather all information about the software
+"""
+
+NAME = "BibtexParser"
+URL = "https://github.com/sciunto/python-bibtexparser"
+LICENSE = "AGPLv3"
+EMAIL = "fboulogne@sciunto.org"
+VERSION = "0.1"
+
+SHORT_DESCRIPTION = "Bibtex parser"
+
+AUHTORS = ["Francois Boulogne <fboulogne@sciunto.org>"]
+
