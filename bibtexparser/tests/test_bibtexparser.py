c19fee0735ad830cf420a869d4ddc0b996f2f85a
François Boulogne <fboulogne sciunto org>
2013-10-01 21:35:19 +0200

    initialize tests for latex encoding

--- bibtexparser/tests/test_bibtexparser.py
+++ bibtexparser/tests/test_bibtexparser.py
@@ -26,6 +26,24 @@ def customizations_unicode(record):
     record = convert_to_unicode(record)
     return record
 
+def customizations_latex(record):
+    """Use all functions related to specific fields
+    + converter to latex.
+
+    :param record: a record
+    :returns: -- customized record
+    """
+
+    record = homogeneize_latex_encoding(record)
+    record = type(record)
+    record = author(record)
+    record = editor(record)
+    record = journal(record)
+    record = keyword(record)
+    record = link(record)
+    record = page_double_hyphen(record)
+    record = doi(record)
+    return record
 
 class TestBibtexParserList(unittest.TestCase):
 
@@ -52,9 +70,29 @@ class TestBibtexParserList(unittest.TestCase):
                          }]
         self.assertEqual(res, expected)
 
-    def test_article_cust(self):
+    def test_article_cust_unicode(self):
         with open('bibtexparser/tests/data/article.bib', 'r') as bibfile:
             bib = BibTexParser(bibfile, customization=customizations_unicode)
+            res = bib.get_entry_list()
+        expected = [{'abstract': 'This is an abstract. This line should be long enough to test\nmultilines... and with a french érudit word',
+                     'type': 'article',
+                     'pages': '12--23',
+                     'volume': '12',
+                     'id': 'Cesar2013',
+                     'year': '2013',
+                     'author': ['César, Jean'],
+                     'journal': {'id': 'NiceJournal', 'name': 'Nice Journal'},
+                     'comments': 'A comment',
+                     'month': 'jan',
+                     'keyword': ['keyword1', 'keyword2'],
+                     'title': 'An amazing title'
+                     }]
+        self.assertEqual(res, expected)
+
+    #@unittest.skip('should improve latex support')
+    def test_article_cust_latex(self):
+        with open('bibtexparser/tests/data/article.bib', 'r') as bibfile:
+            bib = BibTexParser(bibfile, customization=customizations_latex)
             res = bib.get_entry_list()
         expected = [{'abstract': 'This is an abstract. This line should be long enough to test\nmultilines... and with a french érudit word',
                      'type': 'article',
@@ -90,9 +128,26 @@ class TestBibtexParserList(unittest.TestCase):
 
         self.assertEqual(res, expected)
 
-    def test_book_cust(self):
+    def test_book_cust_unicode(self):
         with open('bibtexparser/tests/data/book.bib', 'r') as bibfile:
             bib = BibTexParser(bibfile, customization=customizations_unicode)
+            res = bib.get_entry_list()
+            expected = [{'type': 'book',
+                         'year': '1987',
+                         'edition': '2',
+                         'publisher': 'Wiley Edition',
+                         'id': 'Bird1987',
+                         'volume': '1',
+                         'title': 'Dynamics of Polymeric Liquid',
+                         'author': ['Bird, R.B.', 'Armstrong, R.C.', 'Hassager, O.']
+                         }]
+
+        self.assertEqual(res, expected)
+
+    #@unittest.skip('should improve latex support')
+    def test_book_cust_latex(self):
+        with open('bibtexparser/tests/data/book.bib', 'r') as bibfile:
+            bib = BibTexParser(bibfile, customization=customizations_latex)
             res = bib.get_entry_list()
             expected = [{'type': 'book',
                          'year': '1987',
