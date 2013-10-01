3752508d53a7c69703bf405dd9341a83f1e1ce44
François Boulogne <fboulogne sciunto org>
2013-10-01 20:36:48 +0200

    release

--- bibtexparser/info.py
+++ bibtexparser/info.py
@@ -8,7 +8,7 @@ NAME = "BibtexParser"
 URL = "https://github.com/sciunto/python-bibtexparser"
 LICENSE = "AGPLv3"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "0.3"
+VERSION = "0.4"
 
 SHORT_DESCRIPTION = "Bibtex parser for python3"
 
