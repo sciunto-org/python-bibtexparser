1181e72c5de82976fb3cf1a840ed57bf929410e6
François Boulogne <fboulogne sciunto org>
2014-02-02 09:43:31 -0500

    add __version__

--- bibtexparser/__init__.py
+++ bibtexparser/__init__.py
@@ -1,9 +1,9 @@
 """
 Bibtexparser Module
-"""
-"""
+
 Parser for bibtex files.
 """
 __all__ = ['bparser', 'info', 'latexenc', 'customization']
+__version__ = '0.5'
 
 from . import bparser, info, latexenc, customization
