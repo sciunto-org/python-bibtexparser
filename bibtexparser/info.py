9a5fd886d88eb4860e9f0af468d7e2db6708c681
François Boulogne <fboulogne sciunto org>
2014-04-26 19:05:06 -0400

    fix typo

--- bibtexparser/info.py
+++ bibtexparser/info.py
@@ -8,7 +8,7 @@ NAME = "BibtexParser"
 URL = "https://github.com/sciunto/python-bibtexparser"
 LICENSE = "LGPLv3"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "0.5.3
+VERSION = "0.5.3"
 
 SHORT_DESCRIPTION = "Bibtex parser for python 2 and 3"
 
