52449ffff710fd17d0b7e2633de82864c96f0ee7
François Boulogne <fboulogne sciunto org>
2014-05-31 09:34:32 -0400

    bump to v0.5.5

--- bibtexparser/__init__.py
+++ bibtexparser/__init__.py
@@ -4,6 +4,6 @@ Bibtexparser Module
 Parser for bibtex files.
 """
 __all__ = ['bparser', 'bwrite', 'info', 'latexenc', 'customization']
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 
 from . import bparser, bwriter, info, latexenc, customization
--- bibtexparser/info.py
+++ bibtexparser/info.py
@@ -8,7 +8,7 @@ NAME = "BibtexParser"
 URL = "https://github.com/sciunto/python-bibtexparser"
 LICENSE = "LGPLv3"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "0.5.4"
+VERSION = "0.5.5"
 
 SHORT_DESCRIPTION = "Bibtex parser for python 2 and 3"
 
