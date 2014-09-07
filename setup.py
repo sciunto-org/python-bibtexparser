7d826b976140ecd28d1a21ce608a5c979f32a9e1
François Boulogne <fboulogne sciunto org>
2014-09-06 21:16:28 -0400

    DOC: clarify version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from bibtexparser import info
+from bibtexparser import __version__ as version
 
 setup(
     name         = 'bibtexparser',
-    version      = info.VERSION,
-    url          = info.URL,
-    author       = "Francois Boulogne",
-    license      = info.LICENSE,
-    author_email = info.EMAIL,
-    description  = info.SHORT_DESCRIPTION,
+    version      = version,
+    url          = "https://github.com/sciunto-org/python-bibtexparser",
+    author       = "Francois Boulogne and other contributors",
+    license      = "LGPLv3 or BSD",
+    author_email = "fboulogne@sciunto.org",
+    description  = "Bibtex parser for python 2 and 3",
     packages = ['bibtexparser'],
 )
