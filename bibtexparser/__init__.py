02354ca1bfd676ed0b027ce594d01d7550c68cd6
François Boulogne <fboulogne sciunto org>
2014-09-06 21:44:03 -0400

    switch to devel

--- bibtexparser/__init__.py
+++ bibtexparser/__init__.py
@@ -22,7 +22,7 @@ __all__ = [
     'loads', 'load', 'dumps','dump'
     'bparser', 'bwrite', 'latexenc', 'customization'
 ]
-__version__ = '0.6.0'
+__version__ = 'devel'
 
 from . import bparser, bwriter, latexenc, customization
 
