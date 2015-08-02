ea55e59e484a43caa3d47ff10233d6181479f53a
François Boulogne <fboulogne sciunto org>
2015-08-02 15:49:59 -0400

    v0.6.1

--- bibtexparser/__init__.py
+++ bibtexparser/__init__.py
@@ -22,7 +22,7 @@ __all__ = [
     'loads', 'load', 'dumps', 'dump', 'bibdatabase',
     'bparser', 'bwriter', 'latexenc', 'customization',
 ]
-__version__ = 'devel'
+__version__ = '0.6.1'
 
 from . import bibdatabase, bparser, bwriter, latexenc, customization
 
