0b256a3fd55a62d32fa7356fa9f5800e7cc625c8
Florian Praden <florian@praden.eu>
2016-01-11 12:28:41 +0100

    * add unittest for load/dumps and RAW entity

--- bibtexparser/tests/test_bibtexparser.py
+++ bibtexparser/tests/test_bibtexparser.py
@@ -46,6 +46,58 @@ class TestBibtexParserParserMethods(unittest.TestCase):
             bibtex_database = bibtexparser.load(bibtex_file)
         self.assertEqual(bibtex_database.entries, self.entries_expected)
 
+class TestBibtexParserParserMethodsWithRawEntry(unittest.TestCase):
+    input_file_path = 'bibtexparser/tests/data/book.bib'
+    entries_expected = [{'ENTRYTYPE': 'book',
+                         'RAW' : '@BOOK{Bird1987,\n'
+                         '  title = {Dynamics of Polymeric Liquid},\n'
+                         '  publisher = {Wiley Edition},\n'
+                         '  year = {1987},\n'
+                         '  author = {Bird, R.B. and Armstrong, R.C. and Hassager, O.},\n'
+                         '  volume = {1},\n'
+                         '  edition = {2},\n'
+                         '}', 
+                         'year': '1987',
+                         'edition': '2',
+                         'publisher': 'Wiley Edition',
+                         'ID': 'Bird1987',
+                         'volume': '1',
+                         'title': 'Dynamics of Polymeric Liquid',
+                         'author': 'Bird, R.B. and Armstrong, R.C. and Hassager, O.'
+                        }]
+
+    def test_parse_immediately(self):
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_str = bibtex_file.read()
+        bibtex_database = BibTexParser(bibtex_str, add_raw_bibtex_entry=True)
+        self.assertEqual(bibtex_database.entries, self.entries_expected)
+
+    def test_parse_str(self):
+        parser = BibTexParser(add_raw_bibtex_entry=True)
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_str = bibtex_file.read()
+        bibtex_database = parser.parse(bibtex_str)
+        self.assertEqual(bibtex_database.entries, self.entries_expected)
+
+    def test_parse_file(self):
+        parser = BibTexParser(add_raw_bibtex_entry=True)
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_database = parser.parse_file(bibtex_file)
+        self.assertEqual(bibtex_database.entries, self.entries_expected)
+
+    def test_parse_str_module(self):
+        parser = BibTexParser(add_raw_bibtex_entry=True)
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_str = bibtex_file.read()
+        bibtex_database = bibtexparser.loads(bibtex_str, parser)
+        self.assertEqual(bibtex_database.entries, self.entries_expected)
+
+    def test_parse_file_module(self):
+        parser = BibTexParser(add_raw_bibtex_entry=True)
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_database = bibtexparser.load(bibtex_file, parser)
+        self.assertEqual(bibtex_database.entries, self.entries_expected)
+
 
 class TestBibtexparserWriteMethods(unittest.TestCase):
     input_file_path = 'bibtexparser/tests/data/book.bib'
@@ -70,6 +122,39 @@ class TestBibtexparserWriteMethods(unittest.TestCase):
     def test_write_file(self):
         with open(self.input_file_path) as bibtex_file:
             bibtex_database = bibtexparser.load(bibtex_file)
+
+        with TemporaryFile(mode='w+') as bibtex_out_file:
+            bibtexparser.dump(bibtex_database, bibtex_out_file)
+            bibtex_out_file.seek(0)
+            bibtex_out_str = bibtex_out_file.read()
+
+        self.assertEqual(bibtex_out_str, self.expected)
+
+class TestBibtexparserWriteMethodsWithRawEntity(unittest.TestCase):
+    input_file_path = 'bibtexparser/tests/data/book.bib'
+    expected = \
+"""@book{Bird1987,
+ author = {Bird, R.B. and Armstrong, R.C. and Hassager, O.},
+ edition = {2},
+ publisher = {Wiley Edition},
+ title = {Dynamics of Polymeric Liquid},
+ volume = {1},
+ year = {1987}
+}
+
+"""
+
+    def test_write_str(self):
+        parser = BibTexParser(add_raw_bibtex_entry=True)
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_database = bibtexparser.load(bibtex_file, parser)
+        result = bibtexparser.dumps(bibtex_database)
+        self.assertEqual(result, self.expected)
+
+    def test_write_file(self):
+        parser = BibTexParser(add_raw_bibtex_entry=True)
+        with open(self.input_file_path) as bibtex_file:
+            bibtex_database = bibtexparser.load(bibtex_file, parser)
 
         with TemporaryFile(mode='w+') as bibtex_out_file:
             bibtexparser.dump(bibtex_database, bibtex_out_file)
