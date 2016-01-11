8a1eb8c79b249942b67eac65c6b3dd82f49b9a21
Florian Praden <florian@praden.eu>
2016-01-11 01:24:09 +0100

    * expand bibtexexpression unittest with checking the RAW entry

--- bibtexparser/tests/test_bibtexexpression.py
+++ bibtexparser/tests/test_bibtexexpression.py
@@ -54,3 +54,59 @@ class TestBibtexExpression(unittest.TestCase):
         result = self.expr.entry.parseString(
             '@journal{key, name = "àbcđéf" }')
         self.assertEqual(result.get('Fields'), {'name': 'àbcđéf'})
+
+class TestBibtexExpressionWithRawEntry(unittest.TestCase):
+
+    def setUp(self):
+        self.expr = BibtexExpression(add_raw_bibtex_entry=True)
+
+    def test_minimal(self):
+        result = self.expr.entry.parseString('@journal{key, name = 123 }')
+        self.assertEqual(result.get('EntryType'), 'journal')
+        self.assertEqual(result.get('Key'), 'key')
+        self.assertEqual(result.get('Fields'), {'name': '123'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = 123 }')
+
+    def test_capital_type(self):
+        result = self.expr.entry.parseString('@JOURNAL{key, name = 123 }')
+        self.assertEqual(result.get('EntryType'), 'JOURNAL')
+        self.assertEqual(result.get('RAW'), '@JOURNAL{key, name = 123 }')
+        
+    def test_capital_key(self):
+        result = self.expr.entry.parseString('@journal{KEY, name = 123 }')
+        self.assertEqual(result.get('Key'), 'KEY')
+        self.assertEqual(result.get('RAW'), '@journal{KEY, name = 123 }')
+        
+    def test_braced(self):
+        result = self.expr.entry.parseString('@journal{key, name = {abc} }')
+        self.assertEqual(result.get('Fields'), {'name': 'abc'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = {abc} }')
+
+    def test_braced_with_new_line(self):
+        result = self.expr.entry.parseString(
+            '@journal{key, name = {abc\ndef} }')
+        self.assertEqual(result.get('Fields'), {'name': 'abc\ndef'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = {abc\ndef} }')
+
+    def test_braced_unicode(self):
+        result = self.expr.entry.parseString(
+            '@journal{key, name = {àbcđéf} }')
+        self.assertEqual(result.get('Fields'), {'name': 'àbcđéf'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = {àbcđéf} }')
+
+    def test_quoted(self):
+        result = self.expr.entry.parseString('@journal{key, name = "abc" }')
+        self.assertEqual(result.get('Fields'), {'name': 'abc'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = "abc" }')
+
+    def test_quoted_with_new_line(self):
+        result = self.expr.entry.parseString(
+            '@journal{key, name = "abc\ndef" }')
+        self.assertEqual(result.get('Fields'), {'name': 'abc\ndef'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = "abc\ndef" }')
+
+    def test_quoted_with_unicode(self):
+        result = self.expr.entry.parseString(
+            '@journal{key, name = "àbcđéf" }')
+        self.assertEqual(result.get('Fields'), {'name': 'àbcđéf'})
+        self.assertEqual(result.get('RAW'), '@journal{key, name = "àbcđéf" }')
