fc49004abddc3b2533c97ec7c954a9a3fd2f0e26
Michael Weiss <code@mweiss.ch>
2022-07-09 09:37:33 +0200

    :wrench: Long and short description on pypi (#304)

--- setup.py
+++ setup.py
@@ -12,14 +12,22 @@ with open('bibtexparser/__init__.py') as fh:
             version = line.strip().split()[-1][1:-1]
             break
 
+
+def load_readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
 setup(
-    name         = 'bibtexparser',
-    version      = version,
-    url          = "https://github.com/sciunto-org/python-bibtexparser",
-    author       = "Francois Boulogne and other contributors",
-    license      = "LGPLv3 or BSD",
-    author_email = "devel@sciunto.org",
-    description  = "Bibtex parser for python 2.7 and 3.3 and newer",
-    packages     = ['bibtexparser'],
-    install_requires = ['pyparsing>=2.0.3'],
+    name='bibtexparser',
+    version=version,
+    url="https://github.com/sciunto-org/python-bibtexparser",
+    author="Francois Boulogne and other contributors",
+    license="LGPLv3 or BSD",
+    author_email="devel@sciunto.org",
+    description="Bibtex parser for python 3",
+    long_description_content_type="text/x-rst",
+    long_description=load_readme(),
+    packages=['bibtexparser'],
+    install_requires=['pyparsing>=2.0.3'],
 )
