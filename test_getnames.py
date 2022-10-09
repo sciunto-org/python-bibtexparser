a1cf16fbd1aaa35712ea4f8fd978a50f22883cf1
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2022-10-09 06:57:47 +0000

    remove a test file I created

--- test_getnames.py
+++
@@ -1,5 +0,0 @@
-import bibtexparser
-name = "{G{\'{e}}rard} {Ben Arous}"
-print(name)
-print( bibtexparser.customization.getnames([name]))
-print( bibtexparser.customization.getnames(["{Jean César}"]))
