843018849920e5743f90373b08d325c6daef93e4
Michael Weiss <code@mweiss.ch>
2023-05-26 11:37:35 +0200

    :bug: Expose middlewares

--- bibtexparser/__init__.py
+++ bibtexparser/__init__.py
@@ -1,5 +1,6 @@
 import bibtexparser.exceptions
 import bibtexparser.model
+import bibtexparser.middlewares
 from bibtexparser.entrypoint import parse_file, parse_string, write_file, write_string
 from bibtexparser.library import Library
 from bibtexparser.writer import BibtexFormat
