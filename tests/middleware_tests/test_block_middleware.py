453434fa4f771b58536a7e2f6ac5e9cbaf5c8c03
Matthew Lutze <mlutze@users.noreply.github.com>
2023-07-06 22:55:52 +0200

    add list test

--- tests/middleware_tests/test_block_middleware.py
+++ tests/middleware_tests/test_block_middleware.py
@@ -33,3 +33,55 @@ def test_returning_none_removes_block():
 
     assert library.blocks == []
 
+def test_returning_list_adds_all():
+    class DoubleBlockMiddleware(BlockMiddleware):
+        """A middleware that duplicates every block."""
+        def __init__(self):
+            super().__init__(allow_parallel_execution = True, allow_inplace_modification = True)
+        def transform_block(self, block, library):
+            return [block, block]
+        def metadata_key():
+            return "DoubleBlockMiddleware"
+
+    library = Library(blocks=BLOCKS)
+    library = DoubleBlockMiddleware().transform(library)
+
+    expected = [
+        ExplicitComment("explicit_comment_a"),
+        ExplicitComment("explicit_comment_a"),
+
+        String("string_b", "value_b"),
+        String("string_b", "value_b"),
+
+        String("string_a", "value_a"),
+        String("string_a", "value_a"),
+
+        ImplicitComment("% implicit_comment_a"),
+        ImplicitComment("% implicit_comment_a"),
+
+        ExplicitComment("explicit_comment_b"),
+        ExplicitComment("explicit_comment_b"),
+
+        Entry("article", "entry_a", fields=[]),
+        Entry("article", "entry_a", fields=[]),
+
+        ImplicitComment("% implicit_comment_b"),
+        ImplicitComment("% implicit_comment_b"),
+
+        Entry("article", "entry_b", fields=[]),
+        Entry("article", "entry_b", fields=[]),
+
+        Entry("article", "entry_d", fields=[]),
+        Entry("article", "entry_d", fields=[]),
+
+        Entry("article", "entry_c", fields=[]),
+        Entry("article", "entry_c", fields=[]),
+
+        Preamble("preamble_a"),
+        Preamble("preamble_a"),
+
+        ImplicitComment("% implicit_comment_c"),
+        ImplicitComment("% implicit_comment_c"),
+    ]
+
+    assert library.blocks == expected
