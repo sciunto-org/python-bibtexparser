757bfc9945807a924d202099a7a800a401ca72ac
Juha Jeronen <juha.m.jeronen@gmail.com>
2024-02-23 15:17:13 +0200

    add comment

--- bibtexparser/middlewares/fieldkeys.py
+++ bibtexparser/middlewares/fieldkeys.py
@@ -29,6 +29,22 @@ class NormalizeFieldKeys(BlockMiddleware):
         new_fields_dict: Dict[str, Field] = {}
         for field in entry.fields:
             normalized_key: str = field.key.lower()
+            # Since we always mutate `field` here, we will lose the original key after we normalize it.
+            # Upon a key name conflict, checking here allows us to emit a helpful warning that makes it
+            # easy to locate the offending key even if the entry is long (some online services include
+            # many optional fields into their BibTeX outputs).
+            #
+            # The other option to produce this helpful error message would be to collect a copy of the
+            # original key names, but almost always (no conflicts), they are not needed, so collecting
+            # them would only cause unnecessary pressure on the garbage collector.
+            #
+            # Alternatively, we could just report `entry.key`, not which key or keys were the offending ones,
+            # which runs faster, but is not as helpful for the user.
+            #
+            # Note that maximizing speed here is mainly important in applications where BibTeX parsing takes
+            # much of the run time, such as reference database converters. In most other applications,
+            # whatever the application does with the imported BibTeX data typically takes orders of magnitude
+            # longer than the BibTeX import. For such applications, the better warning message is more important.
             if normalized_key in seen_normalized_keys:
                 logging.warning(
                     f"NormalizeFieldKeys: in entry '{entry.key}': duplicate normalized key '{normalized_key}' (original '{field.key}'); overriding previous value"
