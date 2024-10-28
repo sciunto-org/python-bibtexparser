fcba5adc14f4312d17da80560ea6bfa9a7377079
Robert Birke <robert.birke@unito.it>
2024-10-28 10:37:33 +0100

    Added SeparateCSVLists middleware to split fields with CSV lists

--- bibtexparser/middlewares/__init__.py
+++ bibtexparser/middlewares/__init__.py
@@ -4,6 +4,7 @@ from bibtexparser.middlewares.fieldkeys import NormalizeFieldKeys
 from bibtexparser.middlewares.interpolate import ResolveStringReferencesMiddleware
 from bibtexparser.middlewares.latex_encoding import LatexDecodingMiddleware
 from bibtexparser.middlewares.latex_encoding import LatexEncodingMiddleware
+from bibtexparser.middlewares.lists import SeparateCSVLists
 from bibtexparser.middlewares.middleware import BlockMiddleware
 from bibtexparser.middlewares.middleware import LibraryMiddleware
 from bibtexparser.middlewares.month import MonthAbbreviationMiddleware
